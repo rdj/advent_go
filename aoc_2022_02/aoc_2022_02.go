b3c2e23f08f42d14e50a6b81b683e371945ca15e
Ryan D Johnson <ryandjohnson@gmail.com>
2022-12-01 21:33:35 -0800

    2022-02 cleaned up

--- aoc_2022_02/aoc_2022_02.go
+++ aoc_2022_02/aoc_2022_02.go
@@ -19,7 +19,7 @@ type Part2Result int
 
 const Part2Want = 13_600
 
-type ParsedInput [][]rune
+type ParsedInput [][]int
 
 func openInput() io.Reader {
 	reader, err := os.Open(inputFile)
@@ -30,35 +30,37 @@ func openInput() io.Reader {
 }
 
 func ParseInput(input io.Reader) ParsedInput {
-	lines := make([][]rune, 0)
+	lines := make([][]int, 0)
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
-		line := make([]rune, 2)
+		line := make([]int, 2)
 		_, err := fmt.Sscanf(scanner.Text(), "%c %c", &line[0], &line[1])
 		if err != nil {
 			panic(err)
 		}
+		line[0] -= 'A'
+		line[1] -= 'X'
 		lines = append(lines, line)
 	}
 	return lines
 }
 
+// rock paper scissors
+// outcomes[elf][me] = my score for the round
+var outcomes [][]int = [][]int{
+	{3, 6, 0},
+	{0, 3, 6},
+	{6, 0, 3},
+}
+
+func score(elf, me int) int {
+	return 1 + me + outcomes[elf][me]
+}
+
 func DoPart1(input ParsedInput) Part1Result {
-
-	outcomes := [][]int{
-		{3, 6, 0},
-		{0, 3, 6},
-		{6, 0, 3},
-	}
-
-	//elfscore := 0
 	myscore := 0
 	for _, r := range input {
-		elf := r[0] - 'A'
-		me := r[1] - 'X'
-
-		myscore += 1 + int(me)
-		myscore += outcomes[elf][me]
+		myscore += score(r[0], r[1])
 	}
 
 	return Part1Result(myscore)
@@ -66,28 +68,18 @@ func DoPart1(input ParsedInput) Part1Result {
 
 func DoPart2(input ParsedInput) Part2Result {
 	// lose draw win
+	// choices[elf][desiredOutcome] = my play
 	choices := [][]int{
 		{2, 0, 1},
 		{0, 1, 2},
 		{1, 2, 0},
 	}
 
-	outcomes := [][]int{
-		{3, 6, 0},
-		{0, 3, 6},
-		{6, 0, 3},
-	}
-
-	//elfscore := 0
 	myscore := 0
 	for _, r := range input {
-		elf := r[0] - 'A'
-		choice := r[1] - 'X'
-
-		me := choices[elf][choice]
-
-		myscore += 1 + int(me)
-		myscore += outcomes[elf][me]
+		elf := r[0]
+		outcome := r[1]
+		myscore += score(elf, choices[elf][outcome])
 	}
 
 	return Part2Result(myscore)
